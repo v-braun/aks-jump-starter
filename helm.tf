a54c805cdcca1db80b4c27f76d8f9fc8727d15a7
Viktor Braun (v_b) <mail@viktor-braun.de>
2020-05-04 21:34:49 +0200

    fixed wrong prop path

--- helm.tf
+++ helm.tf
@@ -79,7 +79,7 @@ resource "helm_release" "traefik" {
 # ingressroutes otherwise the letsencrypt generation will fail
 resource "null_resource" "wait_traefik" {
   triggers = {
-    always_run = "${helm_release.traefik.metadata.name}-${helm_release.traefik.metadata.revision}"
+    always_run = "${helm_release.traefik.metadata[0].name}-${helm_release.traefik.metadata[0].revision}"
   }
 
   provisioner "local-exec" {
